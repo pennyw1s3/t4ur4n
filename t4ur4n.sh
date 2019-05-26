2d87f3b6cca680f007717946006458d50f37bc01
pennyw1s3 <50727921+pennyw1s3@users.noreply.github.com>
2019-05-26 19:32:49 +0700

    Add files via upload

---
+++ t4ur4n.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+#Hayo mau ngapain lu bangsd :v
+white='\033[37;1m'
+cyan='\033[36;1m'
+purple='\033[35;1m'
+blue='\033[34;1m'
+yellow='\033[33;1m'
+green='\033[32;1m'
+red='\033[31;1m'
+clear
+echo ""
+
+echo "assalamu'alaikum boejang :)" |lolcat
+sleep 2
+echo "Tak kenal maka tak sayang. "|lolcat
+echo "Maka dari itu tulis nick lu dibawah ini cuk!!!"|lolcat
+read name
+echo "welcome" $name ".Btw, nama ko" $name "Nama lu burik Tq :v"|lolcat
+sleep 1
+echo "Nama gw Tn.Pennywise :v Orang terganss... di masanya"|lolcat
+sleep 1
+echo ""
+
+echo "\033[0;32m
+______________________
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+ ╔════════════╗
+  Tn.PENNYWISE
+ ╚════════════╝
+  ║██████████╚╗
+  ║██╔══╗█╔═╗█║
+  ║██║╬╔╝█╚╗║█║
+  ║██╚═╝█║█╚╝█║
+  ╚╗█████████═╝
+     ╚╗║╠╩╩╩╩╩╝
+       ║║┈┈┈█▐█████▒.｡oO
+       ║██╠╦╦╦╗
+       ╚╗██████";
+
+echo $red " ________     ____     __    __   ______       ____        __      _"  
+echo $red "(___  ___)   (    )    ) )  ( (  (   __ \     (    )      /  \    / )" 
+echo $red "   ) )      / /\ \   ( (    ) )  ) (__) )    / /\ \     / /\ \  / /"  
+echo $red "  ( (      ( (__) )   ) )  ( (  (    __/    ( (__) )    ) ) ) ) ) )"  
+echo $red "   ) )      )    (   ( (    ) )  ) \ \  _    )    (    ( ( ( ( ( ("   
+echo $red "  ( (      /  /\  \   ) \__/ (  ( ( \ \_))  /  /\  \   / /  \ \/ /"   
+echo $red "  /__\    /__(  )__\  \______/   )_) \__/  /__(  )__\ (_/    \__/"    
+echo $yellow "+===========(CORAZON)===========+"
+echo $yellow "|~TOOLS  : TAURAN.              |"
+echo $yellow "|~CODING : Tn.PENNYW1S3.        |"
+echo $yellow "|~CONTAC : 083871259489.        |"
+echo $yellow "|~THANKS TO : DEADPOOL, H4NZ0,  |"
+echo $yellow "| SOLID00, RGS, MARSHALL, DIKA. |"
+echo $yellow "| Mr.H04X & Manteman Bangsd :v. |"
+echo $yellow "|~Special Thanks to Allah SWT   |"
+echo $yellow "+=====[Cyber Wizard Squad]======+"
+echo $blue ">>=========={MENU}==========<<"
+echo "(1).Hammer" | lolcat
+echo $yellow "============================>>"
+echo "(2).Xerxes" | lolcat
+echo $blue "============================>>"
+echo "(3).Torshammer" | lolcat
+echo $yellow "============================>>"
+echo "(4).liteDDOS" | lolcat
+echo $blue "============================>>"
+echo "(5).DDOS TROJAN" | lolcat
+echo $yellow "============================>>"
+echo "(6).HULK" | lolcat
+echo $blue "============================>>"
+echo "(00).Mending lu keluar :v" | lolcat
+echo $yellow "============================>>"
+read -p"pilih nomor:" tpw;
+
+if [ $tpw = 1 ] || [ $tpw = 1 ]
+then
+echo "Lagi nginstall cuk.. -_-!" | lolcat
+pkg update && pkg upgrade
+pkg install python
+pkg install git
+git clone https://github.com/cyweb/hammer
+cd hammer
+chmod +x *
+echo "ketik : cd hammer"|lolcat
+echo "Untuk menjalankan ketik : python hammer.py -s ip target -p port -t 135"|lolcat
+fi
+
+if [ $tpw = 2 ] || [ $tpw = 2 ]
+then
+echo "Tunggu lah asw lagi loading... :v"|lolcat
+pkg update && pkg upgrade
+apt install git
+apt install clang
+git clone https://github.com/zanyarjamal/xerxes
+cd xerxes
+chmod +x *
+echo "ketik : cd xerxes"|lolcat
+echo "untuk menjalankan ketik: ./xerxes (nama website) 80"
+fi
+
+if [ $tpw = 3 ] || [ $tpw = 3 ]
+then
+echo "Tunggu boejang Lagi install"|lolcat
+pkg update && pkg upgrade
+pkg install git
+apt install tor
+pkg install python2
+git clone https://github.com/dotfighter/torshammer.git
+cd torshammer
+chmod +x *
+python2 torshammer.py
+echo "ketik : cd torshammer"|lolcat
+echo "cara menggunakan: python2 torshammer.py"|lolcat
+fi
+
+if [ $tpw = 4 ] || [ $tpw = 4 ]
+then
+echo "lagi install, tunggu ya sayang.. :)"|lolcat
+pkg upgrade && pkg update
+pkg install git
+pkg install python2
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+chmod +x *
+python2 liteDDOS.py
+echo "cara pake ketik: cd LITEDDOS"|lolcat
+echo "Trus ketik: python2 liteDDOS.py"
+fi
+
+if [ $tpw = 5 ] || [ $tpw = 5 ]
+then
+echo "Sabar asw lagi nginstall.. -_-!"|lolcat
+pkg update && pkg upgrade
+apt install python
+apt install bash
+apt install toilet
+apt install lolcat
+git clone https://github.com/BPCATTACKER/DOS-TOOKITS
+cd DOS-TOOKITS
+chmod +x *
+sh DoS-TOOKITS.sh
+echo "cara pake: cd DOS-TOOKITS"|lolcat
+echo "trus: sh DoS-TOOKITS.sh"|lolcat
+fi
+
+if [ $tpw = 00 ] || [ $tpw = 00 ]
+then
+clear
+echo "Sebenarnya ini bukan keluar...:v"|lolcat
+sleep 2
+echo "Tapi Menghapus semua data di memori lu.. :D"|lolcat
+sleep 2
+echo "Mampus lu asw,. wkwkwkkwwk"|lolcat
+sleep 2
+echo "Tapi Boong :v"|lolcat
+sleep 1
+echo "Thanks to:Semua Manteman gw yang ter Unch... -_-"|lolcat
+echo "Dadah Boejang ...:v"|lolcat
+figlet CORAZON|lolcat
+sleep 2
+figlet "Cyber Wizard Squad" |lolcat
+echo "Jangan direcode Asw,_- gw cape bikin nya :v"|lolcat
+echo "Ni contak gw : 083871259489"|lolcat
+echo "Gw Ganss Tq :v"|lolcat
+fi
